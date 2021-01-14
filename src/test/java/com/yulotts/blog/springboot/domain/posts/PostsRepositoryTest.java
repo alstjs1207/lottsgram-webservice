0af5ecf831abf1b46883b2bb879b57171812531b
alstjs1207 <alstjs1207@naver.com>
2021-01-14 23:57:49 +0900

    add posts and postRepository
    게시글 jpa 저장소 만들기
    게시글 엔티티 만들기

---
+++ src/test/java/com/yulotts/blog/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.yulotts.blog.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "테스트게시글";
+        String content = "테스트본문";
+        // id가 있으면 update, 없으면 insert
+        postsRepository.save(Posts.builder()
+                                    .title(title)
+                                    .content(content)
+                                    .author("msyu1207@gmail.com")
+                                    .build());
+        //when
+        // 테이블 posts의 모든 데이터조회
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
