ff9775c401a0dc8604c083f8bceb5fd9887df133
alstjs1207 <alstjs1207@naver.com>
2021-02-21 03:09:21 +0900

    upload image with aws s3

--- src/main/java/com/yulotts/blog/springboot/Application.java
+++ src/main/java/com/yulotts/blog/springboot/Application.java
@@ -1,12 +1,22 @@
 package com.yulotts.blog.springboot;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 //@EnableJpaAuditing //JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
+
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            +"classpath:application.yml,"
+            +"classpath:application-oauth.yml,"
+            +"classpath:aws.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+
+        //SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder(Application.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 }
